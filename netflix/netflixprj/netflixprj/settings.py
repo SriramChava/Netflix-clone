cb33ed3c0eeab6597a894acb2b5893dc20ae704e
SriramChava <sriramchava_2k19ee248@dtu.ac.in>
2022-07-19 03:43:17 +0530

    Added models for Profiles and movies

--- netflix/netflixprj/netflixprj/settings.py
+++ netflix/netflixprj/netflixprj/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+AUTH_USER_MODEL = 'netflixpp.CustomUser'
 
 STATIC_URL = 'static/'
 
