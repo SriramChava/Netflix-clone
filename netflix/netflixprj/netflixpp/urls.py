e0b0433d9eff384d36e4afabd783770c0b02f967
SriramChava <sriramchava_2k19ee248@dtu.ac.in>
2022-07-20 01:36:54 +0530

    Added Movie list, Movie segregation on the basis of maturity, Movie detail page

--- netflix/netflixprj/netflixpp/urls.py
+++ netflix/netflixprj/netflixpp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import Home, ProfileList, ProfileCreate,ProfileDelete
+from .views import Home, ProfileList, ProfileCreate, ProfileDelete, MovieList,MovieDetail
 
 app_name = 'netflixpp'
 
 urlpatterns = [
     path('', Home.as_view(), name='Home'),
     path('profiles/', ProfileList.as_view(), name="profile-list"),
-    path('profiles/create/', ProfileCreate.as_view(), name = "profile-create"),
-    path('profiles/delete/',ProfileDelete.as_view(), name = "profile-delete" )
+    path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
+    path('profiles/delete/', ProfileDelete.as_view(), name="profile-delete"),
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
+    path('watch/detail/<str:movie_id>/',
+         MovieDetail.as_view(), name="movie-detail"),
 ]
--- netflix/netflixprj/netflixpp/views.py
+++ netflix/netflixprj/netflixpp/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .forms import ProfileFormCreate, ProfileFormDelete
-from .models import Profile
+from .models import Profile, Movie
 
 
 class Home(View):
@@ -73,3 +73,40 @@ class ProfileDelete(View):
             'form': form
         }
         return render(request, 'profiledelete.html', context)
+
+
+method_decorator(login_required, name='dispatch')
+
+
+class MovieList(View):
+    def get(self, request, profile_id, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(uuid=profile_id)
+            movies = Movie.objects.filter(age_limit=profile.age_limit)
+            if profile not in request.user.profiles.all():
+                return redirect('netflixpp:profile-list')
+
+            context = {
+                'movies': movies
+            }
+
+            return render(request, 'movielist.html', context)
+        except Profile.DoesNotExist:
+            return redirect('netflixpp:profile-list')
+
+
+method_decorator(login_required, name='dispatch')
+
+
+class MovieDetail(View):
+    def get(self, request, movie_id, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(uuid=movie_id)
+
+            context = {
+                'movie': movie
+            }
+
+            return render(request, 'moviedetail.html', context)
+        except Movie.DoesNotExist:
+            return redirect('netflixpp:profile-list')
