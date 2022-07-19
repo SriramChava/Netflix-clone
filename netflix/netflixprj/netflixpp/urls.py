2e54c9148d6b6e39b82b9152018541510394ff7a
SriramChava <sriramchava_2k19ee248@dtu.ac.in>
2022-07-19 19:07:03 +0530

    Profile list view render

--- netflix/netflixprj/netflixpp/urls.py
+++ netflix/netflixprj/netflixpp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, ProfileList
 
 app_name = 'netflixpp'
 
 urlpatterns = [
-    path('', Home.as_view(), name='Home')
+    path('', Home.as_view(), name='Home'),
+    path('profiles/', ProfileList.as_view(), name="profile-list"),
 ]
--- netflix/netflixprj/netflixpp/views.py
+++ netflix/netflixprj/netflixpp/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 class Home(View):
     def get(self, request, *args, **kwargs):
-        return render(request,'index.html')
+        if request.user.is_authenticated:
+            return redirect('netflixpp:profile-list')
+        return render(request, 'index.html')
+
+
+method_decorator(login_required, name='dispatch')
+
+
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        context = {
+            'profiles': profiles
+        }
+        return render(request, 'profilelist.html', context)
